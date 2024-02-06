9a3f3b892ffdd3f6eb7817184d16399745a3adcd
Vishal G <55740408+vishal-coder@users.noreply.github.com>
2024-02-06 15:11:02 +0530

    Update UseFecth.jsx

--- src/services/UseFecth.jsx
+++ src/services/UseFecth.jsx
@@ -43,3 +43,53 @@ const useFetch = (url, options) => {
 };
 
 export default useFetch;
+
+
+/**
+
+import { useState, useEffect } from "react";
+
+const useFetch = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState(null);
+
+  const fetchData = async (url, options) => {
+    try {
+      setIsLoading(true);
+      const baseURL = import.meta.env.VITE_API_URL;
+      const urlfull = baseURL + "/" + url;
+      const response = await fetch(urlfull, options);
+      const data = await response.json();
+
+      if (!response.ok)
+        throw new Error(
+          JSON.stringify({
+            status: response.status,
+            statusText: response.statusText,
+            message: data.detail,
+          })
+        );
+      setData(data);
+      setErr(null);
+    } catch (err) {
+      console.log("err usefetch", err);
+      setErr(err);
+      setData(null);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // Initial fetch
+    if (url) {
+      fetchData(url, options);
+    }
+  }, [url, options]);
+
+  return { data, isLoading, err, fetchData };
+};
+
+export default useFetch;
+*/
